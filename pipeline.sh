9e5839fa6bc51861180e0e111cc364dd9c762379
Ahsan Raza <aysanraza@gmail.com>
2023-09-26 19:39:20 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,58 +1,31 @@
 echo ""
-echo "================= Welcome to the rapid-virulence pipeline =================" 
+echo "=================Welcome to rapid-virulance: Denovo Bacterical Genome Assembly, Annotation and AMR Pipeline=================" 
 echo ""
-
 echo ""
 echo "Kindly make sure about the specified guidlines:"
 echo ""
-
 echo "1- Project envirnment is installed and activated
 2- You are present in a project folder
-3- Project folder contains the fastq.gz format of read file, .fa format reference sequence, 
-4- Rename the read file as reads.fastq.gz (single end) and reference file as ref.fa
+3- Project folder contains the fastq.gz format of read file/s
+4- Rename the read files as reads.fastq.gz (single end); reads1fastq.gz and reads2.fastq.gz (paired end).
 5- Reads are clean.
 6- Paths are set according to your system"
 echo ""
-
 echo ""
-echo "Choice of Variant Caller:"
-echo ""
-
-echo "1- Alignment based variant calling
-2- Bayesian based variant calling
-3- Exit Program"
-
-echo ""
-read -p "Enter Choice (1 or 2 or 3):" choice
+read -p "Type of Reads (1 or 2):
+1- Single End Read
+2- Paired End Reads" choice
 echo ""
 
 if [ "$choice" == 1 ]; then
-  bowtie2-build project_folder/ref.fa indexfiles
-  bowtie2 -x project_folder/indexfiles -U project_folder/reads.fastq.gz -S project_folder/map.sam
-  head map.sam
-  samtools view -bS project_folder/map.sam > project_folder/map.bam
-  samtools sort project_folder/map.bam -o project_folder/map.sorted.bam
-  bcftools mpileup -f project_folder/ref.fa project_folder/map.sorted.bam | bcftools view -Ov - > project_folder/map.raw.bcf
-  bcftools view project_folder/map.raw.bcf
-  bcftools view -i '%QUAL>=20' map.raw.bcf
-  
-elif [ "$choice" == 2 ]; then
-  bowtie2-build project_folder/ref.fa indexfiles
-  bowtie2 -x project_folder/indexfiles -U project_folder/reads.fastq.gz -S project_folder/ngs_practice/map.sam
-  head map.sam
-  samtools view -bS /project_folder/map.sam > project_folder/map.bam
-  freebayes -f ref.fa map.bam > variants.vcf
-  
-elif [ "$choice" == 3 ]; then
-  echo ""
-  echo "Program Terminated"
-  echo ""
-  exit
-  
+  unicycler -s reads.fastq.gz -o results
 else
-  echo ""
-  echo "Program Terminated due to invalid option selection"
-  echo ""
+  unicycler -s read1.fastq.gz read2.fastq.gz -o results
 fi
 
+prokka project_folder/results/assembly.fasta
+abricate project_folder/results/assembly.fasta > abricate.csv
 
+echo ""
+echo "Program Ended"
+echo ""
